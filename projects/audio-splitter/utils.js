f6944638d8cbc249f2250265e3d8623ebf655610
der_46er <niklas.ausserwoeger@gmail.com>
2026-08-29 23:05:06 +0200

    Delete projects/audio-splitter directory

--- projects/audio-splitter/utils.js
+++
@@ -1,9 +0,0 @@
-function formatTime(sec) {
-    const m = Math.floor(sec / 60).toString().padStart(2, '0');
-    const s = Math.floor(sec % 60).toString().padStart(2, '0');
-    return `${m}:${s}`;
-}
-
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = { formatTime };
-}
